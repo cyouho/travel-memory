bf92c247b06224e035b15e89a188dc2cb29bddb2
zhangfan <cyouho@hotmail.co.jp>
2021-11-30 20:28:28 +0800

    add my home page css

--- resources/views/Home/home_layer.blade.php
+++ resources/views/Home/home_layer.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @include('Global.global_header')
     <link rel="stylesheet" href="/css/page.css">
+    <script src="/js/home_page.js"></script>
     @include('Global.global_navbar')
 </head>
 
