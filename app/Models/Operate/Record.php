b0c7ab9020be940f3d2cb19709c329390b6e21a3
zhangfan <cyouho@hotmail.co.jp>
2021-12-06 08:36:31 +0800

    add travel detail table

--- app/Models/Operate/Record.php
+++ app/Models/Operate/Record.php
@@ -13,6 +13,12 @@ class Record extends Model
     public function insertTravelRecord($data)
     {
         $timestamp = time();
-        $affected = DB::insert('insert into china_province_map_record (user_id, province_adcode, province, city_adcode, city, region_adcode, region, travel_date, record_create_at, record_update_at) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [$data['user_id'], $data['province_adcode'], $data['province'], $data['city_adcode'], $data['city'], $data['region_adcode'], $data['region'], $data['travel_date'], $timestamp, $timestamp]);
+        // $id = DB::insertGetId('insert into china_province_map_record (user_id, province_adcode, province, city_adcode, city, region_adcode, region, travel_date, record_create_at, record_update_at) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [$data['user_id'], $data['province_adcode'], $data['province'], $data['city_adcode'], $data['city'], $data['region_adcode'], $data['region'], $data['travel_date'], $timestamp, $timestamp]);
+
+        $id = DB::table('china_province_map_record')->insertGetId(
+            $data
+        );
+
+        return $id;
     }
 }
