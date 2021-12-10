7b80bb86cddc5553d678c67209e6029b46b3cb98
zhangfan <cyouho@hotmail.co.jp>
2021-12-10 08:24:45 +0800

    add province and city show detail by date

--- resources/views/Province/province_contents.blade.php
+++ resources/views/Province/province_contents.blade.php
@@ -1,3 +1,17 @@
-<div id="china_province">
+<div class="row">
+    <div class="col">
+        <div id="china_province">
 
+        </div>
+    </div>
+    <div class="col">
+        <div id="top"></div>
+        <label for="sel3">日期:</label>
+        <select class="form-control select-width" id="sel3" name="region">
+            <option>近30天</option>
+            <option>近3个月</option>
+            <option>-</option>
+            <option>-</option>
+        </select>
+    </div>
 </div>
